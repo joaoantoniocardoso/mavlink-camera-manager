7dc0b9030599e6e391db395bcaf37e6c463697ec
João Antônio Cardoso <joao.maker@gmail.com>
2022-12-02 21:29:06 -0300

    src: stream: pipeline: Fix freezing when starting WebRTC sinks - Only set play when the first sink is added - Set the h264 profile during v4l pipeline creation

--- src/stream/pipeline/pipeline.rs
+++ src/stream/pipeline/pipeline.rs
@@ -151,15 +151,8 @@ impl PipelineState {
         ))?;
         debug!("Got tee's src pad {:#?}", tee_src_pad.name());
 
-        let pipeline = &self.pipeline;
-
-        // Temporarely set to Ready to avoid losing frames during connection of the new Sink
-        // if let Err(error) = pipeline.set_state(gst::State::Ready) {
-        //     sink.unlink(pipeline, &self.pipeline_id)?;
-        //     bail!(error)
-        // }
-
         // Link the Sink
+        let pipeline = &self.pipeline;
         sink.link(pipeline, &self.pipeline_id, tee_src_pad)?;
         let sink_id = &sink.get_id();
 
@@ -168,10 +161,17 @@ impl PipelineState {
             format!("pipeline-{pipeline_id}-sink-{sink_id}-before-playing"),
         );
 
-        // Start it
-        if let Err(error) = pipeline.set_state(gst::State::Playing) {
-            sink.unlink(pipeline, &self.pipeline_id)?;
-            bail!(error)
+        // Syncronize states
+        if let Err(error) = pipeline.sync_children_states() {
+            bail!("Failed syncronizing Sink elements' state to the Pipeline's state. Reason: {error:#?}");
+        }
+
+        // Start the pipeline if it is the first Sink being added
+        if self.sinks.is_empty() {
+            if let Err(error) = pipeline.set_state(gst::State::Playing) {
+                sink.unlink(pipeline, &self.pipeline_id)?;
+                bail!("Failed starting Pipeline {pipeline_id}. Reason: {error:#?}");
+            }
         }
 
         pipeline.debug_to_dot_file_with_ts(
@@ -219,22 +219,5 @@ impl PipelineState {
         );
 
         Ok(())
-    }
-}
-
-impl Drop for PipelineState {
-    #[instrument(level = "debug")]
-    fn drop(&mut self) {
-        // if let Err(reason) = self.pipeline.set_state(gst::State::Null) {
-        //     warn!("Failed setting Pipeline to NULL. Reason: {reason:#?}");
-        // }
-
-        // let sink_ids = self.sinks.keys().cloned().collect::<Vec<uuid::Uuid>>();
-        // sink_ids.iter().for_each(|sink_id| {
-        //     if let Err(error) = self.remove_sink(sink_id) {
-        //         warn!("{error:#?}");
-        //     }
-        // });
-        self.sinks.clear();
     }
 }
--- src/stream/sink/udp_sink.rs
+++ src/stream/sink/udp_sink.rs
@@ -63,19 +63,6 @@ impl SinkInterface for UdpSink {
             bail!(error)
         }
 
-        // Syncronize states
-        let errors = elements
-            .iter()
-            .filter_map(|element| element.sync_state_with_parent().err())
-            .collect::<Vec<gst::glib::error::BoolError>>();
-        if !errors.is_empty() {
-            pipeline.remove_many(elements)?;
-            tee_src_pad.unlink(queue_sink_pad)?;
-            queue_src_pad.unlink(&self.udpsrc_sink_pad)?;
-
-            bail!("Failed syncronizing Sink elements' state to the Pipeline's state. Errors: {errors:#?}");
-        }
-
         Ok(())
     }
 
