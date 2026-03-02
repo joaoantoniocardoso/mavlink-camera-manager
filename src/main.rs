db6f99e4f83dd409c4ee00a4b46975c5604b7951
João Antônio Cardoso <joao.maker@gmail.com>
2026-03-02 16:01:39 -0300

    Revert "runtime: Use jemalloc allocator and tune Tokio worker threads"
    This reverts commit b2d729466320a338de04f88e3590ef4561c4140c.

--- src/main.rs
+++ src/main.rs
@@ -4,15 +4,11 @@ use mavlink_camera_manager::{
 
 use tracing::*;
 
-#[cfg(target_os = "linux")]
-#[global_allocator]
-static GLOBAL: tikv_jemallocator::Jemalloc = tikv_jemallocator::Jemalloc;
-
 fn main() -> Result<(), std::io::Error> {
     helper::threads::lower_thread_priority();
 
     tokio::runtime::Builder::new_multi_thread()
-        .worker_threads(8)
+        .worker_threads(10)
         .enable_all()
         .on_thread_start(helper::threads::lower_thread_priority)
         .build()
